d418119b6e384f4ed9e7f9c1ea75d68a272f303d
fernaerell <fernaerell2020@gmail.com>
2025-08-13 12:00:07 +0700

    refactor(cmake): rename variable and add file header to Projects.cmake

--- cmake/Projects.cmake
+++ cmake/Projects.cmake
@@ -1,9 +1,25 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) 2025, The Aerell Language Project Authors
+# All rights reserved.
+# 
+# This file is part of the Aerell Language project.
+# 
+# Licensed under the BSD 3-Clause License. 
+# You may obtain a copy of the License at:
+# https://github.com/aerell-lang/aerell/blob/main/LICENSE
+# 
+# File: Projects.cmake
+# Description: Contains the list of all subprojects and their inclusion order.
+# Author: Fern Aerell fernaerell2020@gmail.com
+# Created: 2025-08-13
+# -----------------------------------------------------------------------------
+
 set(
-    PROJECT_DIR_NAMES
+    SUBPROJECT_DIR_NAMES
     "aerell"
     "argp"
 )
 
-foreach(PROJECT_DIR_NAME ${PROJECT_DIR_NAMES})
-    add_subdirectory(${PROJECT_DIR_NAME})
+foreach(SUBPROJECT_DIR_NAME ${SUBPROJECT_DIR_NAMES})
+    add_subdirectory(${SUBPROJECT_DIR_NAME})
 endforeach()
