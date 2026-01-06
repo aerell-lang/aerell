1bf0e6b7e5f02adf3dc140c333a27400022e0ee1
fernaerell <fernaerell2020@gmail.com>
2026-01-07 05:23:14 +0700

    feat: add header and source file for file manager

---
+++ include/aerell/file/file_manager.h
@@ -0,0 +1,7 @@
+// Copyright 2026 Fern Aerell.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef AERELL_FILE_FILE_MANAGER_H
+#define AERELL_FILE_FILE_MANAGER_H
+
+#endif
---
+++ src/aerell/file/file_manager.c
@@ -0,0 +1,4 @@
+// Copyright 2026 Fern Aerell.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "aerell/file/file_manager.h"
