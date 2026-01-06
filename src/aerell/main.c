f2c6539cfe3c921d0d8845771be5fcc7cd5bd079
fernaerell <fernaerell2020@gmail.com>
2026-01-07 05:22:34 +0700

    feat: Change main source with simple program to load file and tokenizing with simple lexer and print the result

--- src/aerell/main.c
+++ src/aerell/main.c
@@ -3,8 +3,29 @@
 
 #include <stdio.h>
 
-int main()
+#include "aerell/lexer.h"
+#include "aerell/file/file.h"
+
+int main(int argc, const char* argv[])
 {
-    printf("Hello World\n");
+    if(argc == 1) return 0;
+
+    const char* path = argv[1];
+
+    file_t* file = file_load(path);
+    if(file == NULL)
+    {
+        printf("Failed to load file %s\n", path);
+        return 1;
+    }
+
+    token_t tokens[10] = {0};
+    size_t size = sizeof(tokens) / sizeof(tokens[0]);
+
+    lexer(file->content, tokens, size);
+
+    print_tokens(tokens);
+
+    file_free(file);
     return 0;
 }
