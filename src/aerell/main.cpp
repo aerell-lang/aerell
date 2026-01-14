ac637b71ac1a780b2a89b3193195e25b4109731f
fernaerell <fernaerell2020@gmail.com>
2026-01-15 05:20:01 +0700

    refactor: Implementing new ir and module in main function

--- src/aerell/main.cpp
+++ src/aerell/main.cpp
@@ -1,11 +1,14 @@
 // Copyright 2026 Fern Aerell.
 // SPDX-License-Identifier: Apache-2.0
 
+#include <print>
+
 #include "aerell/file.hpp"
 #include "aerell/lexer.hpp"
 #include "aerell/parser.hpp"
 #include "aerell/semantic.hpp"
 #include "aerell/ir.hpp"
+#include "aerell/module.hpp"
 
 int main()
 {
@@ -13,7 +16,9 @@ int main()
     aerell::Lexer lexer{file};
     aerell::Parser parser{lexer};
     aerell::Semantic semantic{parser};
-    aerell::IR::gen(semantic);
+    aerell::IR ir{semantic};
+    aerell::Module module = ir.gen();
+    std::println("{}", module.toStr());
 
     return 0;
 }
