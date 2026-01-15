207d369f9590f5a2352fd21fc85e708e3de059e6
fernaerell <fernaerell2020@gmail.com>
2026-01-15 12:11:52 +0700

    feat: Add error code

---
+++ include/aerell/error_code.hpp
@@ -0,0 +1,29 @@
+// Copyright 2026 Fern Aerell.
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include <algorithm>
+#include <array>
+#include <cstdint>
+#include <string_view>
+
+namespace aerell
+{
+
+enum class ErrorCode : std::uint8_t
+{
+    E0,
+};
+
+constexpr auto errorCodeStr = std::to_array<std::pair<ErrorCode, std::string_view>>({
+    {ErrorCode::E0, "E0"},
+});
+
+constexpr std::string_view toStr(ErrorCode k)
+{
+    auto it = std::find_if(errorCodeStr.begin(), errorCodeStr.end(), [k](const auto& p) { return p.first == k; });
+    return (it != errorCodeStr.end()) ? it->second : "UNKNOWN";
+}
+
+} // namespace aerell
