5eb50d8cbcf3a3844d3b9bc0147dec8c415c9f26
fernaerell <fernaerell2020@gmail.com>
2026-01-15 08:59:07 +0700

    feat: Add struct lexeme

---
+++ include/aerell/token/lexeme.hpp
@@ -0,0 +1,12 @@
+// Copyright 2026 Fern Aerell.
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include <cstdint>
+
+struct Lexeme
+{
+    std::uint32_t offset = 0;
+    std::uint32_t size = 0;
+};
