78f8bda8f26c8b6c2b2fc84be302e4f69983db5b
fernaerell <fernaerell2020@gmail.com>
2026-01-25 21:08:18 +0700

    feat: Make llvm ir gen can print number literal directly and add llvm ir verify

--- src/aerell/llvm/llvm_ir_gen.c
+++ src/aerell/llvm/llvm_ir_gen.c
@@ -1,9 +1,13 @@
 // Copyright 2026 Fern Aerell
 // SPDX-License-Identifier: Apache-2.0
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <stddef.h>
 
 #include <llvm-c/Core.h>
+#include <llvm-c/Analysis.h>
 
 #include "aerell/llvm/llvm_ir_gen.h"
 
@@ -55,19 +59,73 @@ void llvm_ir_gen_generate(llvm_ir_gen_t* self, const ir_mod_t* ir_mod)
         LLVMBasicBlockRef entry = LLVMAppendBasicBlockInContext(context, func, "entry");
         LLVMPositionBuilderAtEnd(builder, entry);
 
+        uint32_t args_cap = 2;
+        uint32_t args_qty = 0;
+        LLVMValueRef* args = malloc(sizeof(LLVMValueRef) * args_cap);
+        if(args == NULL) continue;
+
         for(uint32_t j = 0; j < inst_qty; j++)
         {
             switch(ir_mod->funcs.inst_type[i][j])
             {
-            case IR_FUNC_INST_TYPE_ARG: break;
-            case IR_FUNC_INST_TYPE_CALL: break;
+            case IR_FUNC_INST_TYPE_ARG:
+                if(args_qty >= args_cap)
+                {
+                    uint32_t nargs_cap = args_cap * 2;
+                    LLVMValueRef* nargs = realloc(args, sizeof(LLVMValueRef) * nargs_cap);
+                    if(nargs == NULL) continue;
+                    args_cap = nargs_cap;
+                    args = nargs;
+                }
+
+                switch(ir_mod->vals.type[ir_mod->funcs.inst_data[i][j].arg.val_idx])
+                {
+                case IR_TYPE_VOID: break;
+                case IR_TYPE_I32:
+                    int32_t val_i32 = ir_mod->vals.data[ir_mod->funcs.inst_data[i][j].arg.val_idx].i32;
+                    args[ir_mod->funcs.inst_data[i][j].arg.arg_idx] =
+                        LLVMConstInt(LLVMInt32TypeInContext(context), val_i32, 0);
+                    break;
+                case IR_TYPE_STR:
+                    const char* val_c_str = ir_mod->vals.data[ir_mod->funcs.inst_data[i][j].arg.val_idx].str;
+                    LLVMValueRef val_llvm_str = LLVMConstStringInContext2(context, val_c_str, strlen(val_c_str), 0);
+                    LLVMTypeRef val_llvm_type = LLVMTypeOf(val_llvm_str);
+                    LLVMValueRef g_var = LLVMAddGlobal(module, val_llvm_type, "");
+                    LLVMSetInitializer(g_var, val_llvm_str);
+                    LLVMSetGlobalConstant(g_var, 1);
+                    args[ir_mod->funcs.inst_data[i][j].arg.arg_idx] = g_var;
+                    break;
+                }
+
+                args_qty++;
+                break;
+            case IR_FUNC_INST_TYPE_CALL:
+                LLVMValueRef func =
+                    LLVMGetNamedFunction(module, ir_mod->vals.data[ir_mod->funcs.inst_data[i][j].idx].str);
+                LLVMTypeRef type = LLVMGlobalGetValueType(func);
+                LLVMBuildCall2(builder, type, func, args, args_qty, "");
+
+                args_qty = 0;
+                break;
             }
         }
+
+        args_cap = 0;
+        args_qty = 0;
+        free(args);
+        args = NULL;
 
         LLVMBuildRetVoid(builder);
     }
 
     LLVMDumpModule(module);
+
+    char* error = NULL;
+    if(LLVMVerifyModule(module, LLVMReturnStatusAction, &error))
+    {
+        printf("%s\n", error);
+        LLVMDisposeMessage(error);
+    }
 
     LLVMDisposeBuilder(builder);
     LLVMDisposeModule(module);
