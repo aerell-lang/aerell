75dbe646f6de9a8da4c3cb5a1ef611694487de1f
fernaerell <fernaerell2020@gmail.com>
2026-01-25 13:24:53 +0700

    refactor: Change name cmod to c_mod

--- src/aerell/backend/c/c_gen.c
+++ src/aerell/backend/c/c_gen.c
@@ -14,9 +14,9 @@ typedef struct
     char* data;
     uint32_t cap;
     uint32_t qty;
-} cmod_t;
+} c_mod_t;
 
-void cmod_write(cmod_t* self, const char* val)
+void c_mod_write(c_mod_t* self, const char* val)
 {
     assert(self != NULL && val != NULL && "self or val is null");
 
@@ -34,7 +34,7 @@ void cmod_write(cmod_t* self, const char* val)
     self->data[self->qty] = '\0';
 }
 
-void cmod_free(cmod_t* self)
+void c_mod_free(c_mod_t* self)
 {
     assert(self != NULL && "self is null");
 
@@ -48,72 +48,72 @@ const char* c_gen_generate(const ir_mod_t* mod)
 {
     assert(mod != NULL && "mod is null");
 
-    cmod_t cmod = {0};
+    c_mod_t c_mod = {0};
 
     for(uint32_t i = 0; i < mod->funcs.qty; i++)
     {
-        if(mod->funcs.pub[i] && mod->funcs.inst_qty[i] == 0) cmod_write(&cmod, "extern ");
+        if(mod->funcs.pub[i] && mod->funcs.inst_qty[i] == 0) c_mod_write(&c_mod, "extern ");
         switch(mod->funcs.ret[i])
         {
-        case IR_TYPE_VOID: cmod_write(&cmod, "void "); break;
-        case IR_TYPE_I32: cmod_write(&cmod, "int "); break;
-        case IR_TYPE_STR: cmod_write(&cmod, "const char* "); break;
+        case IR_TYPE_VOID: c_mod_write(&c_mod, "void "); break;
+        case IR_TYPE_I32: c_mod_write(&c_mod, "int "); break;
+        case IR_TYPE_STR: c_mod_write(&c_mod, "const char* "); break;
         }
-        cmod_write(&cmod, mod->vals.data[mod->funcs.name[i]].str);
-        cmod_write(&cmod, "(");
+        c_mod_write(&c_mod, mod->vals.data[mod->funcs.name[i]].str);
+        c_mod_write(&c_mod, "(");
         for(uint32_t j = 0; j < mod->funcs.param_qty[i]; j++)
         {
-            if(j > 0) cmod_write(&cmod, ", ");
+            if(j > 0) c_mod_write(&c_mod, ", ");
             switch(mod->funcs.param[i][j])
             {
-            case IR_TYPE_VOID: cmod_write(&cmod, "void"); break;
-            case IR_TYPE_I32: cmod_write(&cmod, "int"); break;
-            case IR_TYPE_STR: cmod_write(&cmod, "const char*"); break;
+            case IR_TYPE_VOID: c_mod_write(&c_mod, "void"); break;
+            case IR_TYPE_I32: c_mod_write(&c_mod, "int"); break;
+            case IR_TYPE_STR: c_mod_write(&c_mod, "const char*"); break;
             }
         }
-        if(mod->funcs.vrdic[i]) cmod_write(&cmod, ",...");
-        cmod_write(&cmod, ")");
+        if(mod->funcs.vrdic[i]) c_mod_write(&c_mod, ",...");
+        c_mod_write(&c_mod, ")");
         if(mod->funcs.inst_qty[i] == 0)
         {
-            cmod_write(&cmod, ";");
+            c_mod_write(&c_mod, ";");
             continue;
         }
-        cmod_write(&cmod, "{");
-        cmod_t arg = {0};
+        c_mod_write(&c_mod, "{");
+        c_mod_t arg = {0};
         for(uint32_t j = 0; j < mod->funcs.inst_qty[i]; j++)
         {
             switch(mod->funcs.inst_type[i][j])
             {
             case IR_FUNC_INST_TYPE_ARG:
-                if(mod->funcs.inst_data[i][j].arg.arg_idx > 0) cmod_write(&arg, ",");
+                if(mod->funcs.inst_data[i][j].arg.arg_idx > 0) c_mod_write(&arg, ",");
                 switch(mod->vals.type[mod->funcs.inst_data[i][j].arg.val_idx])
                 {
                 case IR_TYPE_VOID: break;
                 case IR_TYPE_I32:
                     char x[12];
                     snprintf(x, sizeof(x), "%i", mod->vals.data[mod->funcs.inst_data[i][j].arg.val_idx].i32);
-                    cmod_write(&arg, x);
+                    c_mod_write(&arg, x);
                     break;
                 case IR_TYPE_STR:
-                    cmod_write(&arg, "\"");
-                    cmod_write(&arg, mod->vals.data[mod->funcs.inst_data[i][j].arg.val_idx].str);
-                    cmod_write(&arg, "\"");
+                    c_mod_write(&arg, "\"");
+                    c_mod_write(&arg, mod->vals.data[mod->funcs.inst_data[i][j].arg.val_idx].str);
+                    c_mod_write(&arg, "\"");
                     break;
                 }
                 break;
             case IR_FUNC_INST_TYPE_CALL:
-                cmod_write(&cmod, mod->vals.data[mod->funcs.name[mod->funcs.inst_data[i][j].idx]].str);
-                cmod_write(&cmod, "(");
-                cmod_write(&cmod, arg.data);
-                cmod_write(&cmod, ");");
-                cmod_free(&arg);
+                c_mod_write(&c_mod, mod->vals.data[mod->funcs.name[mod->funcs.inst_data[i][j].idx]].str);
+                c_mod_write(&c_mod, "(");
+                c_mod_write(&c_mod, arg.data);
+                c_mod_write(&c_mod, ");");
+                c_mod_free(&arg);
                 break;
             }
         }
-        cmod_free(&arg);
-        cmod_write(&cmod, "}");
-        cmod_write(&cmod, "int main(){init();return 0;}");
+        c_mod_free(&arg);
+        c_mod_write(&c_mod, "}");
+        c_mod_write(&c_mod, "int main(){init();return 0;}");
     }
 
-    return cmod.data;
+    return c_mod.data;
 }
